0b77e7b7f99da46637ba54154cfcfa89ca1e4962
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-04-27 10:50:42 +0100

    Adds jest test

--- Code/express-server/controllers/book.test.js
+++ Code/express-server/controllers/book.test.js
@@ -1,19 +1,65 @@
 import { getAllBooks } from "../bookControllers.js";
-
+import { jest } from "@jest/globals";
 import supertest from "supertest";
 import app from "../app.js";
 
-test("that i get all the books on a successful query", () => {
-  // // Mock
-  // const req = {};
-  // const res = {};
-  // jest.spyOn(req);
-  // jest.spyOn(res);
-  // // sinon
-  // const response = getAllBooks(req, res);
-  // console.log(response);
-  // // Spy
-  // expect(true).toBe(true);
+const mockRequest = (query = {}, body) => ({
+  query: query,
+  body,
+});
+
+const mockResponse = () => {
+  const res = {};
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn();
+  return res;
+};
+
+test("that i get all the books on a successful query", async () => {
+  const req = mockRequest();
+  const res = mockResponse();
+
+  await getAllBooks(req, res);
+
+  expect(res.json).toHaveBeenCalled();
+  const returnValue = res.json.mock.calls[0][0];
+
+  expect(returnValue.length).toBe(2);
+  expect(returnValue).toContainEqual({
+    id: 1,
+    author: "John Scalzi",
+    title: "Old Man's War",
+  });
+
+  expect(returnValue).toContainEqual({
+    id: 2,
+    author: "Mary Robinette Kowal",
+    title: "The Calculating Stars",
+  });
+});
+
+test("that i filter the books on a correct query", async () => {
+  const req = mockRequest({ author: "John" });
+  const res = mockResponse();
+
+  await getAllBooks(req, res);
+
+  expect(res.json).toHaveBeenCalled();
+  const returnValue = res.json.mock.calls[0][0];
+
+  expect(returnValue.length).toBe(1);
+  console.log(returnValue);
+  expect(returnValue).toContainEqual({
+    id: 1,
+    author: "John Scalzi",
+    title: "Old Man's War",
+  });
+
+  expect(returnValue).not.toContainEqual({
+    id: 2,
+    author: "Mary Robinette Kowal",
+    title: "The Calculating Stars",
+  });
 });
 
 test("that given the right query parameter, the right object will be returned", async () => {
---
+++ Code/express-server/controllers/validationMiddleware.js
@@ -0,0 +1,6 @@
+function anyFunction(req, res, next) {
+  // do some logic and decide can you continue
+  // invoke next
+  // otherwise
+  // send a response or raise an error.
+}
